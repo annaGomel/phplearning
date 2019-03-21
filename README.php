1750ea08bbeef815313ae7af29a979789350007c
Web <web@it.step>
2019-03-21 21:36:11 +0300

    first commit

---
+++ README.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php echo '" Khal Drogo\'s favorite word is "atjahakar" "'; ?>
+    <br>
+    <?php
+    $name = "Peter<br>";
+    echo "Hello " .$name;
+    var_dump($name);
+    
+    $numberOne = 5;
+    $numberThwo = 9;
+    echo $numberOne * $numberThwo . "<br>";
+  
+    $name = "Anna";
+    $name2 = "Mary";
+    echo $name[0] . $name2[0];
+    ?>
+
+</body>
+
+</html>
