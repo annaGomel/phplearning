ff21c9ee561d2087b2cc75d907eca365b1d56a92
annaGomel <anutik88@yandex.ru>
2019-04-04 21:29:53 +0300

    04042019.class

--- lesson4/formy.php
+++ lesson4/formy.php
@@ -10,5 +10,29 @@ if(isset($_POST['fruits'])){
 } else {
     echo "ne otmechen";
 }
-    
+
+//2.	Сделайте функцию input, которая создает инпут с типом text. Функция
+// должна иметь следующие параметры: name, value.
+
+function input($type, $name, $value){
+    return "<input type ={$type};
+                   name ={$name};
+                   value ={$value}>";
+};
+    echo input($_POST['type'],
+           $_POST['name'],
+           $_POST['value']);
+
+
+//3.	Спросите у пользователя имя с помощью формы. Сделайте чекбокс: если он отмечен, то поприветствуйте
+//пользователя, если не отмечен - попрощайтесь с пользователем.-->
+$value = $_POST['name'];
+$login = $_POST['login'];
+if($value == 1){
+    echo "Privet, ";
+     echo($login);
+    } else {
+        echo "poka!";
+    }
+
 ?>
